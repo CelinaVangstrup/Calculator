d4d81741a262f2a9d1338304b11acea9095f9f90
Celina Vangstrup <au682953@uni.au.dk>
2022-02-03 13:09:46 +0100

    Add project files.

---
+++ Calculator/SimpleCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class SimpleCalculator
+    {
+
+        public double Add(double a, double b)
+        {
+            var addResult = a + b;
+
+            return addResult;
+        }
+        public double Subtract(double a, double b)
+        {
+            var subResult = a - b;
+
+            return subResult;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            var multiResult = a * b;
+
+            return multiResult;
+        }
+
+        public double Power(double x, double exp)
+        {
+            var powerResult = Math.Pow(x, exp);
+            return powerResult;
+
+        }
+    }
+}
---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System.Runtime.InteropServices;
+using Calculator;
+using NUnit.Framework;
+
+
+namespace TestProject
+{
+    public class Tests
+    {
+        private SimpleCalculator _uut;
+
+        [SetUp] 
+        public void Setup()
+        {
+            _uut = new SimpleCalculator();
+        }
+
+        [TestCase(5)]
+        public void AddTest(double expected)
+        {
+            double sum = _uut.Add(2, 3);
+            Assert.That(expected, Is.EqualTo(sum));
+        }
+    }
+}
