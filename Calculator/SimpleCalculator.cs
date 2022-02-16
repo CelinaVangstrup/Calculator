3f28090e492b825b5971e95202a563885c2e7f88
JeppeWJ <202007790@post.au.dk>
2022-02-16 14:56:35 +0100

    Exception fjernet.

--- Calculator/SimpleCalculator.cs
+++ Calculator/SimpleCalculator.cs
@@ -4,59 +4,59 @@ using System.Text;
 
 namespace Calculator
 {
-    public class SimpleCalculator
-    {
-        public double Accumulate { get; private set; } = 0;
-        
-        public double Add(double a, double b)
-        {
-            var addResult = a + b;
+   public class SimpleCalculator
+   {
+      public double Accumulate { get; private set; } = 0;
 
-            Accumulate = addResult;
-            return addResult;
-        }
-        public double Subtract(double a, double b)
-        {
-            var subResult = a - b;
+      public double Add(double a, double b)
+      {
+         var addResult = a + b;
 
-            Accumulate = subResult;
-            return subResult;
-        }
+         Accumulate = addResult;
+         return addResult;
+      }
+      public double Subtract(double a, double b)
+      {
+         var subResult = a - b;
 
-        public double Multiply(double a, double b)
-        {
-            var multiResult = a * b;
+         Accumulate = subResult;
+         return subResult;
+      }
 
-            Accumulate = multiResult;
-            return multiResult;
-        }
+      public double Multiply(double a, double b)
+      {
+         var multiResult = a * b;
 
-        public double Power(double x, double exp)
-        {
-            var powerResult = Math.Pow(x, exp);
+         Accumulate = multiResult;
+         return multiResult;
+      }
 
-            Accumulate = powerResult;
-            return powerResult;
-        }
+      public double Power(double x, double exp)
+      {
+         var powerResult = Math.Pow(x, exp);
 
-        public double Divide(double dividend, double divisor)
-        {
-            
-           if (divisor!=0)
-           {
-               var divideResult = dividend / divisor;
-               Accumulate = divideResult;
-               return divideResult;
-           }
-           else
-           {
-              throw new Exception("Du prøvede at dividere med nul");
-           }
-        }
+         Accumulate = powerResult;
+         return powerResult;
+      }
 
-        public void Clear()
-        {
-            Accumulate = 0;
-        }
-    }
+      public double Divide(double dividend, double divisor)
+      {
+
+         //if (divisor!=0)
+         //{
+         var divideResult = dividend / divisor;
+         Accumulate = divideResult;
+         return divideResult;
+         //}
+         //else
+         //{
+         //   throw new Exception("Du prøvede at dividere med nul");
+         //}
+      }
+
+      public void Clear()
+      {
+         Accumulate = 0;
+      }
+   }
 }
